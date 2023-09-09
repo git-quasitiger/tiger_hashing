0da93d8b9e52992a0899f06c0619a5414e0a20c5
git-quasitiger <140503414+git-quasitiger@users.noreply.github.com>
2023-09-09 15:05:49 +0900

    Update hash_digest.py
    remove hash calculate function

--- hash_digest.py
+++ hash_digest.py
@@ -1,15 +1,12 @@
 import hashlib
 # 바이너리 데이터를 입력받아 md5 해시값을 반환하는 함수
 def md5_digest(_data) -> str:
-    md5_checksum = hashlib.md5(_data).hexdigest()
-    return md5_checksum
+    pass
 
 # 바이너리 데이터를 입력받아 sha1 해시값을 반환하는 함수
 def sha1_digest(_data) -> str:
-    sha1_checksum = hashlib.sha1(_data).hexdigest()
-    return sha1_checksum
+    pass
 
 # 바이너리 데이터를 입력받아 sha256 해시값을 반환하는 함수
 def sha256_digest(_data) -> str:
-    sha256_checksum = hashlib.sha256(_data).hexdigest()
-    return sha256_checksum
+    pass
